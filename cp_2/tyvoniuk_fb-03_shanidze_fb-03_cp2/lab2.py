e3368665ab01a7fe673077c2fafc70745192c26e
David <71350591+teebeeaf@users.noreply.github.com>
2022-11-26 13:57:51 +0200

    alpha 0.1.1

--- cp_2/tyvoniuk_fb-03_shanidze_fb-03_cp2/lab2.py
+++ cp_2/tyvoniuk_fb-03_shanidze_fb-03_cp2/lab2.py
@@ -1,4 +1,3 @@
-
 print("Task 1")
 
 alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
@@ -31,7 +30,7 @@ for key in (key_0, key_1, key_2, key_3, key_jestkiy):
 print("Task 2")
 
 
-def super_index(text, alphabet):
+def super_index(text: str, alphabet):
     an = 0
     n = len(text)
     for i in alphabet:
@@ -49,3 +48,29 @@ for i in CT:
 
 print("Відкритий текст:")
 print(super_index(text, alphabet))
+
+print("Task 3")
+
+with open('finaltextXD.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+
+def rozbivka_na_gramy(text, n):
+    temp = []
+    for i in range(n):
+        j = i
+        l = ""
+        while j < len(text):
+            l += text[j]
+            j += n
+        temp.append(l)
+
+    return temp
+
+mb_kluchi = []
+for i in range(2, 31):
+    mb_kluchi.append(rozbivka_na_gramy(text, i))
+
+for i in mb_kluchi:
+    print(i)
+    print(super_index(i, alphabet))
