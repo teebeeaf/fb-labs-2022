fa010db02eb42138c5be5cc443428df1d5d47486
David <71350591+teebeeaf@users.noreply.github.com>
2022-12-03 12:56:19 +0200

    added lab2

---
+++ cp_2/tyvoniuk_fb-03_shanidze_fb-03_cp2/edit.py
@@ -0,0 +1,22 @@
+with open('text.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+text.lower()
+
+a = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+print(len(a))
+answer = ''
+for i in text:
+    if i in a:
+        answer += i
+    if i == '\n':
+        answer += ''
+    # if i == 'ъ':
+    #     answer += 'ь'
+    if i == 'ё':
+        answer += 'е'
+
+answer = ' '.join(list(answer.split()))
+
+with open('finaltext.txt', 'w', encoding='utf-8') as file:
+    file.write(answer)
