d22dafb81554187f00f33753ec4bf38aa8f171c9
UvonuctV <tyvoniuk.volodymyr.fb03@gmail.com>
2022-12-07 22:16:14 +0200

    progress

--- cp_3/tyvoniuk_fb-03_shanidze_fb-03_cp3/lab3.py
+++ cp_3/tyvoniuk_fb-03_shanidze_fb-03_cp3/lab3.py
@@ -14,17 +14,45 @@ def convert_bigram(text: str):
         i += 2
     return symbols
 
+def euclid_rev(a: int, b: int):
+    return pow(a, -1, b)
+
+def linear_gcd(a: int, b: int):
+    rest = a % b
+    while rest != 0:
+        a = b
+        b = rest
+        rest = a - (b * int(a / b))
+    return b
+
+def linear_euclid(a: int, b: int):
+    a = a%b
+    mod = b
+    if a == 0:
+        return 0
+    nodes = [1, 0]
+    while b != 0:
+        nodes = [nodes[1], nodes[0] - (a // b) * nodes[1]]
+        a, b = b, a % b
+    return nodes[0] % mod
+
+def linear_equations(a: int, b: int, mod: int):
+    possible_a = []
+    gcd = linear_gcd(a % mod, b % mod)
+    if gcd == 1:
+        possible_a.append(linear_euclid(a,n))
+
+
+
+print(linear_gcd(35,5))
+
+print(linear_euclid(608,961))
+
+print(euclid_rev(608,961))
+
 # def linear_equations(a, b, n):
 #         if a
 
-
-
-def euclid_perdic(a: int, b: int):
-    while
-    div = b // a
-    rest = b % a
-
-    return y1 - (b // a) * x1
 
 
 def decrypt(text):
@@ -69,7 +97,11 @@ def func1():
             a_x, b_x = decrypt(j[0]), decrypt(j[1])
             a = decrypt(j[0]) - decrypt(v[0]) #X* - X** (a)
             b = decrypt(j[1]) - decrypt(v[1]) #Y* - Y** (b)
-            hernya = euclid_perdic(a, mod)
+            possible_a = linear_equations(a, b, mod)
+            hernya = euclid_rev(a, mod)
+
+
+
             gcd, inverse = hernya[0] % mod, hernya[1]
             print(gcd)
             result = []
@@ -106,9 +138,5 @@ b = list(sorted(bigrams(text).items()))
 b.sort(key=lambda x: -x[1])
 print(b[:10])
 
-# print(func1())
+# # print(func1())
 
-print(euclid_perdic(12, 961))
-print(euclid_perdic(58, 961))
-print(euclid_perdic(301, 961))
-print(euclid_perdic(452, 961))
